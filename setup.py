ab627ddb38a420e3bcd2798295c3a1ccb4ae2c65
krexspace <38793905+krexspace@users.noreply.github.com>
2020-11-27 16:44:35 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='krystal',
-version='1.1.1',
+version='1.1.2',
 description='Image processing for Deep Learning',
 url='https://github.com/krexspace/krystal.git',
 author='krex',
