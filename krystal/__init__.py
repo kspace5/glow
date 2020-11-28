09eb59c425e371202a257a955ad6ce1591080420
krexspace <38793905+krexspace@users.noreply.github.com>
2020-11-27 17:27:52 -0800

    Update __init__.py

--- krystal/__init__.py
+++ krystal/__init__.py
@@ -4,7 +4,9 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
 from .image import *
-from . import imagetrans
+from .imagetrans import *
+from .video import *
+
 
 
 
