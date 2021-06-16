bd36de16d6c89b90eebf1cc138915af93d0f7376
krex <krexspace@gmail.com>
2021-06-16 15:39:26 -0700

    version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='glow',
-version='1.4.1',
+version='1.4.2',
 description='Image processing for Deep Learning',
 url='https://github.com/krexspace/glow.git',
 author='krex',
